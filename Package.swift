a4b1ab8fd56fd69406a302958ee2d81c3c3da592
Sebastian Husche <sebastian@scanbot.io>
2021-06-23 16:28:57 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "ScanbotBarcodeScannerSDK",
             url: "https://download.scanbot.io/barcode-scanner-sdk/ios/xcframeworks/scanbot-ios-barcode-scanner-sdk-xcframework-1.12.0.zip",
-            checksum: "4c2d5633fd37b59a7cba4f01f3b4f8a195b7d0be50b8112cfaadd4419a9488e9"
+            checksum: ""
         ),
     ]
 )
