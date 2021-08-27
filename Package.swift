a63a0ded7f8b41ff7bb3dd643ece496eb4edade4
Daniil Voitenko <daniil@scanbot.io>
2021-08-27 10:32:51 +0200

    Publishing SPM package for release v3.0.2.

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ScanbotBarcodeScannerSDK",
-            url: "https://download.scanbot.io/barcode-scanner-sdk/ios/xcframeworks/scanbot-ios-barcode-scanner-sdk-xcframework-3.0.1.zip",
-            checksum: "812a65e8f7016ddc1acbc98367b4ef850bb0fa5e35a215cef371f50e8ce8a1d3"
+            url: "https://download.scanbot.io/barcode-scanner-sdk/ios/xcframeworks/scanbot-ios-barcode-scanner-sdk-xcframework-3.0.2.zip",
+            checksum: "52616b89318ec1a6ef5de92f014685782ebd82c6af64e7934cc81daa31c5fac4"
         ),
     ]
 )
