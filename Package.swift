3466d1dae12ed53fd757a52c214e4d295c7dc81c
scanbot-ci <ci@scanbot.io>
2024-05-10 12:43:44 +0200

    Publishing SPM package for release v5.1.0-RC11.

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ScanbotBarcodeScannerSDK",
-            url: "https://download.scanbot.io/barcode-scanner-sdk/ios/pre/xcframeworks/RC10/scanbot-ios-barcode-scanner-sdk-xcframework-5.1.0.zip",
-            checksum: "60792b43f64fed2b030511e7fc8926e7fcc21ba1326b6b3e5f1673c0cd4076e3"
+            url: "https://download.scanbot.io/barcode-scanner-sdk/ios/pre/xcframeworks/RC11/scanbot-ios-barcode-scanner-sdk-xcframework-5.1.0.zip",
+            checksum: "1abdf4dd21d0e17f705ce7abf30ae6d706437ef3b1711c67f40a05cc67f9169f"
         ),
     ]
 )
