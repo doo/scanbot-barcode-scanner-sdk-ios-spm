f252ee4a7c4ec8afa51a68eb99b9b6afa267adbc
scanbot-ci <ci@scanbot.io>
2024-07-15 16:58:33 +0200

    Publishing SPM package for release v5.2.1-Beta2.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "ScanbotBarcodeScannerSDK",
             url: "https://download.scanbot.io/barcode-scanner-sdk/ios/pre/xcframeworks/Beta2/scanbot-ios-barcode-scanner-sdk-xcframework-5.2.1.zip",
-            checksum: "4fe463b9243d913ab6f190b65bf306a6694e8b727beeecd14e512ae0af87591c"
+            checksum: "5e17dfab01f8a7355b92b6c851fc2445baedca311b8395ccbcd4c5ec1fcdf842"
         ),
     ]
 )
